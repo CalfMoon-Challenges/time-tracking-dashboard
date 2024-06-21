8be1a8fda15e7ec7e7056cddf2bba88086b41e4c
Kritagya Bhattarai <Kritagya Bhattarai (CalfMoon)>
2024-06-21 21:06:58 +0545

    feat: add basic structure to heading section

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,9 +1,35 @@
+import React from "react";
 import "./UserCard.css";
+import usericon from "./assets/images/image-jeremy.png";
 
 export default function () {
+  const timeList = ["Daily", "Weekly", "Monthly"];
+
   return (
     <header>
-      <div></div>
+      <div className="user-box">
+        <img src={usericon} alt="" className="user-box__usericon" />
+        <p className="user-box__statement">
+          Report for
+          <span className="user-box__statement__name"> Jeremy Robson </span>
+        </p>
+      </div>
+
+      <div className="time-selector">
+        {timeList.map((value, index) => (
+          <React.Fragment key={value}>
+            <input
+              type="radio"
+              className="time-selector__time"
+              name="selected-time"
+              id={value}
+              value={value}
+              defaultChecked={index === 0}
+            />
+            <label htmlFor={value}>{value}</label>
+          </React.Fragment>
+        ))}
+      </div>
     </header>
   );
 }
