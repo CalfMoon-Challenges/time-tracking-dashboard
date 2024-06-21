c320f5bce2077b1aa879f90d7d9f362cf681539e
Kritagya Bhattarai <Kritagya Bhattarai (CalfMoon)>
2024-06-21 13:50:51 +0545

    initial commit

---
+++ src/MainBody.jsx
@@ -0,0 +1,9 @@
+import "./MainBody.css";
+
+export default function () {
+  return (
+    <main>
+      <div></div>
+    </main>
+  );
+}
---
+++ src/UserCard.jsx
@@ -0,0 +1,9 @@
+import "./UserCard.css";
+
+export default function () {
+  return (
+    <header>
+      <div></div>
+    </header>
+  );
+}
